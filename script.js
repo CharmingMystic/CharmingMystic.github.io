1b31c18c2743ada4ffe99e98328d1f10306a0b5b
CharmingMystic <113670387+CharmingMystic@users.noreply.github.com>
2023-02-28 01:13:13 +0100

    Update script.js

--- script.js
+++ script.js
@@ -4,19 +4,26 @@ function showAlert() {
  
 
 
-var map = L.map('map').setView([53.7531, 15.6229], 9);
+window.onload = function() 
+ // Initialize map
+  var map = L.map('map').setView([53.7531, 15.6229], 9);
 
-L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+  // Add tile layer to map
+  L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors',
     maxZoom: 18,
-}).addTo(map);
-var marker = L.marker([53.4289, 14.5528]).addTo(map);
-marker.bindPopup("<b>Stargard</b><br>Zachodniopomorskie");
+  }).addTo(map);
 
-var circle = L.circle([53.5804, 16.6378], {
+  // Add marker to map
+  var marker = L.marker([53.4289, 14.5528]).addTo(map);
+  marker.bindPopup("<b>Stargard</b><br>Zachodniopomorskie");
+
+  // Add circle to map
+  var circle = L.circle([53.5804, 16.6378], {
     color: 'red',
     fillColor: '#f03',
     fillOpacity: 0.5,
     radius: 5000
-}).addTo(map);
-circle.bindPopup("Koszalin<br>Zachodniopomorskie");
+  }).addTo(map);
+  circle.bindPopup("Koszalin<br>Zachodniopomorskie");
+});
